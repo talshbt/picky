03259b77987e1b17cbd9d5e822dc5b77d5905e9e
talshbt <talshbt@gmail.com>
2020-03-14 10:25:00 +0200

    4 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { ClothingComponent } from './clothing/clothing.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule],
   declarations: [ AppComponent, HelloComponent, HeaderComponent, ClothingComponent, ShoppingListComponent ],
   bootstrap:    [ AppComponent ]
 })

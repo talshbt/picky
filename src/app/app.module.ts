68c049aaaca00ea411bda295c599c9cfbb29df8e
talshbt <talshbt@gmail.com>
2020-03-14 12:23:04 +0200

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule } from "@angular/material";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { ClothingListComponent } from './clothing/clothing-list/clothing-list.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, ClothingComponent, ShoppingListComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ClothingComponent, ShoppingListComponent, ClothingListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

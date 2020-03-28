2031441c37dae53dee2eca3be586c35129fdf002
talshbt <talshbt@gmail.com>
2020-03-28 10:41:55 +0300

    1 file modified, 2 added, 1 deleted

--- src/app/modals/add-clothes/add-clothes.component.ts
+++ src/app/modals/add-clothes/add-clothes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-clothes',
   templateUrl: './add-clothes.component.html',
-  styleUrls: ['./add-clothes.component.css']
+  styleUrls: ['./add-clothes.component.scss']
 })
 export class AddClothesComponent implements OnInit {
 
